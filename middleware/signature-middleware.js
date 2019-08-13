d04c2a0c7f3f1cc6784bd433ad172dc73a2e62ad
sannonthachai <sannonthachai@gmail.com>
2019-08-13 13:11:34 +0700

    Create signature middleware

---
+++ middleware/signature-middleware.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto')
+
+module.exports = {
+    compareSignature: async (req, res, next) => {
+
+        const channelSecret = "e8d9ca27da8f627c7712f9fb11a4a1ca"
+        const body = JSON.stringify(req.body)
+        const signature = crypto.createHmac('SHA256', channelSecret).update(body).digest('base64')
+
+        if (req.headers["x-line-signature"] !== signature) {
+
+            return res.status(500)
+        }
+            
+        return next()
+    }
+}
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,38 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const crypto = require('crypto')
 const request = require('request')
 const optionsClass = require('../config/request-option')
+const middleware = require('../middleware/signature-middleware')
 
 /* GET users listing. */
-router.post('/webhook', (req, res, next) => {
-  
-  const channelSecret = "e8d9ca27da8f627c7712f9fb11a4a1ca"
-  const body = JSON.stringify(req.body)
-  const signature = crypto.createHmac('SHA256', channelSecret).update(body).digest('base64')
-
-  if (req.headers["x-line-signature"] !== signature) {
-
-    return res.status(500)
-  }
-
+router.post('/webhook', middleware.compareSignature, (req, res) => {
   // res.status(200).json({ message: "Success!" });
-  // console.log(req.headers["x-line-signature"])
   console.log(req.body.events[0].message)
 
   req.body.events.forEach(element => {
-
     // console.log(JSON.stringify(element.replyToken))
     let options = new optionsClass(element.replyToken)
 
-    request(options, (error, res, next) => {
+    request(options, (error, res) => {
       if (!error && res.statusCode == 200) {
-        // console.log("success")
-        return next
+        console.log("success")
       }
     })
   })
-
   // console.log(req.body.events[0].type)
 });
 
